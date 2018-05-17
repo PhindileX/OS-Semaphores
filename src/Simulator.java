ed6c8d6b412f4691ffa42eca6726d6b2642baba0
PhindileX <xphindile5@gmail.com>
2018-05-17 16:24:51 +0200

    Added a Simulator.java class which is a driver class for the program

---
+++ src/Simulator.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+public class Simulator{
+
+    public static void main(String[] args){
+        int noOfPeople;
+        int noOfBranches;
+        Arraylist<String[]> peopleAndStops = new ArrayList<String[]>();
+        File file = new File("../lib/input.txt");
+        Scanner inputFile = new Scanner(file);
+        noOfPeople = input.getNext();
+        noOfBranches = input.getNext();
+        while(inputFile.hasNext()){
+            peopleAndStops.add(input.getNext());
+        }
+        
+    }
+}
